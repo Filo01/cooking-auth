2b31ed1d7c191f4f6dd435d32cc2605cb6c7cfba
Filo01 <github@filippo.pw>
2024-09-17 21:26:48 +0000

    feat: removed permission to GET all users

--- project/users/permissions.py
+++ project/users/permissions.py
@@ -12,9 +12,5 @@ class IsUserOrCreatingAccountOrReadOnly(permissions.BasePermission):
         if user_is_making_new_account:
             return True
 
-        is_read_only_action = request.method in permissions.SAFE_METHODS
-        if is_read_only_action:
-            return True
-
         is_accessing_their_own_user_object = obj == request.user
         return is_accessing_their_own_user_object
