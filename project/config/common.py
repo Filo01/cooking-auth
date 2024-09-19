dc637b557030cd5959854018fb92474a97d6043c
Filippo <github@filippo.pw>
2024-09-19 11:45:17 +0200

    feat: add send mail for OTP (#5)
    * feat: add send mail for OTP
    
    - added throttling to prevent mail spam
    
    * feat: added test configuration
    
    - added test configuration
    - removed throttling for tests
    
    * fix: github action test config

--- project/config/common.py
+++ project/config/common.py
@@ -199,6 +199,11 @@ class Common(Configuration):
             "rest_framework_simplejwt.authentication.JWTAuthentication",
             # "project.users.authentication.CustomJWTAuthentication",
         ),
+        "DEFAULT_THROTTLE_CLASSES": [
+            "rest_framework.throttling.AnonRateThrottle",
+            "rest_framework.throttling.UserRateThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {"anon": "10/hour", "user": "1000/day"},
     }
 
     SPECTACULAR_SETTINGS = {
